3faf9101e5edff77b58c087905c6d4dcabbba1d2
1203743133 <1296394143@qq.com>
2019-02-13 17:15:10 +0800

    你好

--- fyy/src/main/java/Hello.java
+++ fyy/src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String args[]){
 
-        System.out.println("Hi Worldfanyanyan2");
+        System.out.println("Hi Worldfanyanyan你好");
 
     }
 }
